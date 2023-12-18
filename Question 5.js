54ae9c3cda8efd15d8a0c051af8a5b128756f609
Amir Zahedi <amirzahedi185@gmail.com>
2023-12-17 17:47:32 -0800

    Question 5 - Print out the details of an Object

---
+++ Question 5.js
@@ -0,0 +1,9 @@
+// Question 5
+const person = {
+    name : "Foo",
+    age : 20,
+    address : "example"
+};
+function PerInfo(){
+    console.log(person.name + " " + person.age + " " + person.address);
+};
