1d0b54f95251adb9390df9c53b52347b9167ccbd
Amir Zahedi <amirzahedi185@gmail.com>
2023-12-17 17:50:27 -0800

    Question 7 - Add a new property to all object

---
+++ Question 7.js
@@ -0,0 +1,12 @@
+// Question 7
+const car = {
+    start(){
+        console.log("the car has started");
+    }
+};
+const person = {
+    name : "Foo",
+    age : 20,
+    address : "example"
+};
+Object.prototype.height = 100;
